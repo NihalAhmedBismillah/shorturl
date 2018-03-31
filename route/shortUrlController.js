7dd0ad384e503ac692b83b748363e288068e3c15
unknown <nihaltomail@gmail.com>
2018-04-01 00:49:57 +0530

    Implemented App cache

--- route/shortUrlController.js
+++ route/shortUrlController.js
@@ -2,7 +2,8 @@
  * Import npm modules
  */
 const shortUrl = require('./../model/shortUrl');
-
+const NodeCache = require("node-cache");
+const appCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 class ShortUrlController {
 
@@ -14,26 +15,55 @@ class ShortUrlController {
 
                 shortUrl.saveShortUrl(req).then((data) => {
                     res.render('main/index', { link: data.shortUrl });
-                }).catch((error)=>{
+                }).catch((error) => {
 
                     res.status(404).send('Not found!');
                 });
             });
             app.get('/:shorturl', (req, res) => {
 
-                shortUrl.getShortUrls(req).then((data) => {
-                    if (data && data.longUrl) {
-                        res.redirect(data.longUrl);
+                //Check in local cache.....
+                if (!req.params && !req.params.shorturl) {
+                    return res.status(40).send('Invalid paramater');
+                }
+                const shortid = req.params.shorturl;
+                //get cache
+                appCache.get(shortid, (error, value) => {
+
+                    if (!error && value) {
+                        console.log('<=======Getting form App cache=====>');
+                        res.redirect(value);
+
                     } else {
-                        res.status(404).send('Not found!');
+                        shortUrl.getShortUrls(req).then((data) => {
+
+                            if (data && data.longUrl) {
+                                //set cache 
+                                appCache.set(shortid, data.longUrl, (error, status) => {
+                                    if (!error && status)
+                                        console.log('<=======App cache set=======>');
+                                    else
+                                        console.log('<=======App cache not set=======>');
+                                });
+                                res.redirect(data.longUrl);
+                            } else {
+                                res.status(404).send('Not found!');
+                            }
+                        }).catch((error) => {
+                            res.status(404).send('Not found!');
+                        });
                     }
-                }).catch((error) => {
-                    res.status(404).send('Not found!');
+
                 });
             });
             app.get('/listshorturl', (req, res, next) => {
 
                 res.send('ok');
+            });
+            app.get('/api/v1/remove/appcache', (req, res, next) => {
+                appCache.flushAll();
+                appCache.getStats();
+                res.status(200).send({ data: 'App cache deleted!' });
             });
             resolve(true);
         });
